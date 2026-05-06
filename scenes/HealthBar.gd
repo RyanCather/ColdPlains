6af9f96ce35c7a4cde01b4aa70a9b0ed42528283
Reign-Dears <0502314@schoolsnet.act.edu.au>
2026-05-06 13:57:58 +1000

    Update HealthBar.gd

--- scenes/HealthBar.gd
+++ scenes/HealthBar.gd
@@ -25,3 +25,4 @@ func connect_to_local_player():
 func _on_health_changed(new_health):
 	print("Health updated:", new_health)  # debug
 	health_bar.value = new_health
+#this if for my vet compedency 
