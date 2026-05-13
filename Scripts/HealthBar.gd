386aab012be24da44565797e0642f48c5689e3b1
Reign-Dears <0502314@schoolsnet.act.edu.au>
2026-05-13 14:11:07 +1000

    health bar update
    Added a seperate healthbar at the bottom, im going to be changing the grey colour of the background to a green that also depletes, have to also get rd of the top healthbar that isnt functional.

--- Scripts/HealthBar.gd
+++ Scripts/HealthBar.gd
@@ -1,24 +1,11 @@
 extends CanvasLayer
 
-@onready var health_bar = $Healthbar
-@onready var health_label = $Healthbar/HealthLabel
+@onready var health_label = $HealthLabel
 
 
 func _ready():
-	# Configure the progress bar to show health 0-100
-	health_bar.min_value = 0
-	health_bar.max_value = 100
-	health_bar.value = 100
-	
-	connect_to_local_player()
-	
-	# Configure the progress bar to show health 0-100
-	health_bar.min_value = 0
-	health_bar.max_value = 100
-	health_bar.value = 100
-	
-	# Set bar colors directly
-	health_bar.self_modulate = Color.GREEN  # Makes the bar green
+	# Set label color to green
+	health_label.add_theme_color_override("font_color", Color.GREEN)
 	
 	connect_to_local_player()
 
@@ -35,10 +22,10 @@ func connect_to_local_player():
 			player.health_changed.connect(_on_health_changed)
 			
 			# Set initial value
-			health_bar.value = player.health
 			health_label.text = str(player.health) + " / 100"
+
+
 
 func _on_health_changed(new_health):
 	print("Health updated:", new_health)  # debug
-	health_bar.value = new_health
 	health_label.text = str(new_health) + " / 100"
